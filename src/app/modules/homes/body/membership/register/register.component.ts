1df3d5c25e221aa98f63b35be5d22fb7b8e7be62
jeonyeohun <21500630@handong.edu>
2020-10-29 14:05:22 +0900

    [FEAT] Create markup and styles for register page

--- src/app/modules/homes/body/membership/register/register.component.ts
+++ src/app/modules/homes/body/membership/register/register.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthEmailService } from '../../../../communications/fe-backend-db/membership/auth-email.service';//register user service
 import { Router } from '@angular/router'
-class userProfile{
-  nickName : String;
-  name : String;
-  inst : String;//institution
-  email : String;
-  password : String;
+class userProfile {
+  nickName: String;
+  name: String;
+  position: String;
+  inst: String;//institution
+  email: String;
+  password: String;
 }
 
 
@@ -21,38 +22,34 @@ export class RegisterComponent implements OnInit {
 
   // when input info(email and password), add data into object.
   registerUserData = new userProfile();
-  private pw1="";
-  private pw2="";
+  private pw1 = "";
+  private pw2 = "";
   constructor(private eAuth: AuthEmailService, private _router: Router) { }
 
   ngOnInit() {
     this.initialize_data();
   }
 
-  initialize_data(){
+  initialize_data() {
     this.pw1 = "";
     this.pw2 = "";
     this.registerUserData = new userProfile();
   }
 
-  checkIfValid(){
-    // console.log(this.pw1)
-    // console.log(this.pw2)
+  checkIfValid() {
     return this.pw1 == this.pw2;
   }
 
   // when button clicked, this func init.
-  async registerUser(){
-    if(this.pw1 == this.pw2)//check for sure
+  async registerUser() {
+    if (this.pw1 == this.pw2)//check for sure
       this.registerUserData.password = this.pw1;
-    console.log(this.registerUserData);
+
     await this.eAuth.register(this.registerUserData) //_auth : register user service
     this._router.navigate(['/homes'])
-    // console.log(this.registerUserData);
-    // this.eAuth.register(this.registerUserData) //_auth : register user service
   }
-  
-  toSocReg(){
+
+  toSocReg() {
     this._router.navigateByUrl("/membership/socReg");
   }
 
