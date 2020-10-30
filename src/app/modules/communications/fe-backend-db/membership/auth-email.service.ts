75ddba9fd922ad6dae3cf3b00e8626156ff8b617
jeonyeohun <21500630@handong.edu>
2020-10-30 19:33:00 +0900

    [FIX] Fix wrong routing after register

--- src/app/modules/communications/fe-backend-db/membership/auth-email.service.ts
+++ src/app/modules/communications/fe-backend-db/membership/auth-email.service.ts
@@ -1,5 +1,5 @@
 import { UserProfile, logStat } from './user.model';
-import {Auth} from "./userAuth.model";
+import { Auth } from "./userAuth.model";
 import { HttpClient } from "@angular/common/http";
 import { Injectable, Injector } from "@angular/core";
 import { Router } from "@angular/router";
@@ -11,22 +11,22 @@ class storeToken {
     //property coverage should be considered more... use private?
     type: logStat;
     token: string;
-  
+
     constructor(type: logStat, token: string) {
-      this.type = type;
-      this.token = token
+        this.type = type;
+        this.token = token
     }
-  }
+}
 
 // import { profile } from 'console';
 // import { QueryServiceService } from '../query-service.service';
 @Injectable({
     providedIn: 'root'
 })
-export class AuthEmailService extends  Auth{
+export class AuthEmailService extends Auth {
 
     protected URL = this.ipService.get_FE_DB_ServerIp();
-    protected user : UserProfile;
+    protected user: UserProfile;
     private EMAIL_REG_URL = this.URL + "/eUser/register"; //mongoDB
     private EMAIL_LOGIN_URL = this.URL + "/eUser/login";
     private EMAIL_VERIFY_TOKEN = this.URL + "/eUser/verify";
@@ -58,7 +58,7 @@ export class AuthEmailService extends  Auth{
         return this.user;
         // throw new Error("Method not implemented.");
     }
-    
+
     /**
     * @function getInstance()
     * @returns email auth 인스턴스를 반환. 싱글턴 패턴 사용.
@@ -81,21 +81,16 @@ export class AuthEmailService extends  Auth{
 
         // let isOurUser$ = this.eCheckUser(user);
         // let res = await isOurUser$.toPromise();
-        let isOurUser = await super.postIsOurUser(user,this.EMAIL_CHECK_OUR_USER_URL);
+        let isOurUser = await super.postIsOurUser(user, this.EMAIL_CHECK_OUR_USER_URL);
         //console.log(isOurUser);
         if (isOurUser.succ) {//if this user is one of us, deny registration.
             alert("이미 등록되어 있는 id 입니다. 로그인 페이지로 이동합니다.");
             //비밀번호 찾기 페이지도 만들어야 한다. 
-            this.router.navigateByUrl("/membership/login");
+            this.router.navigateByUrl("/login");
         }
         else {
             let res = await this.http.post<any>(this.EMAIL_REG_URL, user).toPromise();
-            
-            // //console.log(res)
-            alert("환영합니다, " + res.payload.name + "님. 홈 화면으로 이동합니다.");
             return res.succ;
-            // this.auth.confirmUser(logStat.email, res);
-            // err => console.log(err)
         }
 
     }
@@ -104,7 +99,7 @@ export class AuthEmailService extends  Auth{
     async logIn(user): Promise<any> {
         //console.log("login req user : ", user);
 
-        let isOurUser = await super.postIsOurUser(user,this.EMAIL_CHECK_OUR_USER_URL);
+        let isOurUser = await super.postIsOurUser(user, this.EMAIL_CHECK_OUR_USER_URL);
         // console.log(isOurUser);
         if (!isOurUser.succ) {//if this user is one of us, deny registration.
             alert("아직 KUBiC 회원이 아니시군요? 회원가입 해주세요! :)");
@@ -117,9 +112,9 @@ export class AuthEmailService extends  Auth{
             // login succ
             if (res.succ) {
                 alert("돌아오신 걸 환영합니다, " + res.payload.name + "님. 홈 화면으로 이동합니다.");
-                console.log("answer : ",res)
+                console.log("answer : ", res)
                 localStorage.setItem('token', JSON.stringify(new storeToken(logStat.email, res.payload.token)));
-                this.user = new UserProfile(logStat.email,res.payload.email,res.payload.name,res.payload.token)
+                this.user = new UserProfile(logStat.email, res.payload.email, res.payload.name, res.payload.token)
                 console.log("result : ", this.user);
                 super.confirmUser(this.user);
                 // return { logStat: logStat.email, token: res.payload.token, name: res.payload.name, email: res.payload.email };
