b32f0213fb3dbc0a2fe440d575df14ef6d03ccfc
jeonyeohun <21500630@handong.edu>
2020-11-07 14:26:35 +0900

    [FEAT] Renew registration page by adding angular validations

--- src/app/modules/homes/body/membership/register/register.component.ts
+++ src/app/modules/homes/body/membership/register/register.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthEmailService } from '../../../../communications/fe-backend-db/membership/auth-email.service';//register user service
 import { Router } from '@angular/router'
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 class userProfile {
   nickName: String;
   name: String;
-  position: String;
-  inst: String;//institution
+  status: String;
+  inst: String;
   email: String;
   password: String;
 }
@@ -18,34 +19,67 @@ class userProfile {
 })
 
 export class RegisterComponent implements OnInit {
-  constructor(private eAuth: AuthEmailService, private _router: Router) { }
 
-  registerUserData = new userProfile();
-  private pw1 = "";
-  private pw2 = "";
+  private registerForm: FormGroup;
+  private userProfile = new userProfile();
+  private statusList: any = ['대학생', '석사', '박사', '연구원', '기타'];
 
-  ngOnInit() {
-    this.initialize_data();
+  constructor(
+    private eAuth: AuthEmailService,
+    private _router: Router,
+    private formBuilder: FormBuilder,
+  ) { }
+
+  ngOnInit(): void {
+    this.registerForm = this.formBuilder.group({
+      name: new FormControl('', [
+        Validators.required,
+      ]),
+      nickName: new FormControl('', [
+        Validators.required,
+      ]),
+      status: new FormControl('', [
+        Validators.required,
+      ]),
+      inst: new FormControl('', [
+        Validators.required,
+      ]),
+      email: new FormControl('', [
+        Validators.required,
+        Validators.email,
+      ]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(8),
+      ]),
+      passwordConfirm: new FormControl('', [
+        Validators.required,
+      ]),
+    }, {
+      validator: this.mustMatch('password', 'passwordConfirm'),
+    });
   }
 
-  initialize_data() {
-    this.pw1 = "";
-    this.pw2 = "";
-    this.registerUserData = new userProfile();
-  }
+  onSubmit() {
 
-  checkIfValid() {
-    return this.pw1 == this.pw2;
+    console.log(this.registerForm.get('name').value);
+    this.userProfile.name = this.registerForm.get('name').value;
+    this.userProfile.nickName = this.registerForm.get('nickName').value;
+    this.userProfile.status = this.registerForm.get('status').value;
+    this.userProfile.inst = this.registerForm.get('inst').value;
+    this.userProfile.email = this.registerForm.get('email').value;
+    this.userProfile.password = this.registerForm.get('password').value;
+
+
+
+    this.registerUser();
   }
 
   async registerUser() {
-    if (this.pw1 == this.pw2) //check for sure
-      this.registerUserData.password = this.pw1;
-
-    let regResult = await this.eAuth.register(this.registerUserData); //_auth : register user service
+    let regResult = await this.eAuth.register(this.userProfile); //_auth : register user service
 
     if (regResult) {
-      this._router.navigate(['/register-ok'], { queryParams: { email: this.registerUserData.email } });
+      this._router.navigate(['/register-ok'], { queryParams: { email: this.userProfile.email } });
     }
     else {
       window.location.reload();
@@ -56,4 +90,21 @@ export class RegisterComponent implements OnInit {
     this._router.navigateByUrl("/socReg");
   }
 
+  mustMatch(controlName: string, matchingControlName: string) {
+    return (formGroup: FormGroup) => {
+      const control = formGroup.controls[controlName];
+      const matchingControl = formGroup.controls[matchingControlName];
+
+      if (matchingControl.errors && !matchingControl.errors.mustMatch) {
+        return;
+      }
+
+      if (control.value !== matchingControl.value) {
+        matchingControl.setErrors({ mustMatch: true });
+      }
+      else {
+        matchingControl.setErrors(null);
+      }
+    }
+  }
 }
