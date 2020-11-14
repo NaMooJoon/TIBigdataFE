0cadd09f35e28d6defeb7c28a33c71459afada44
jeonyeohun <21500630@handong.edu>
2020-11-14 21:01:53 +0900

    [FEAT] Add 'getUserInfo': query to get document data from mongo

--- fe-backend/module/googleUserQuery.js
+++ fe-backend/module/googleUserQuery.js
@@ -1,9 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/user');
-// const conn = require('./connection/userConn')
-// const User = conn.model('user',require('./models/User'))
-//google token verify code template
 const { OAuth2Client } = require('google-auth-library');
 const Res = require("../models/Res");
 
@@ -33,7 +30,7 @@ function verifyGoogleToken(req, res) {
         // }
         // const userid = payload['sub'];
         // If request specified a G Suite domain:
-        //const domain = payload['hd'];
+        //const domain = payload['hd'];  
 
         return res.status(200).send(new Res(true, "google verify succ", { status: true, user: payload }));
     }
@@ -46,24 +43,46 @@ function verifyGoogleToken(req, res) {
 }
 
 router.post('/gRegister', (req, res) => {
-    // console.log("api : gRegister init.");
     let userData = req.body;
     userData.auth = "google";
     let user = new User(userData);
     user.save((error, registeredUser) => {
+        console.log(error, registeredUser);
         if (error) {
             console.log("google social user register data save error : " + error);
             res.json(new Res(false, "google resgister fail"));
         }
         else {
-            // console.log("api : gmail register : save ok");
+            console.log("api : gmail register : save ok");
             res.json(new Res(true, "google register ok", { succ: true, user : user.email }));
         }
     })
 })
 
+router.post('/getUserInfo', (req, res) =>{
+    let userData = req.body;
+    let userEmail = userData.email;
+
+    console.log("email: " + userEmail);
+
+    User.findOne({ email: userEmail }, (error, user) => {
+        if (error){
+            console.err(error);
+        }
+        else {
+            if (!user) { 
+                res.json(new Res(false, 'danger'));
+            }
+            else{
+                console.log(user);
+                res.json(new Res(true, 'User authenticated!', { name: user.name, email: user.email, inst: user.inst, nickname: user.nickName, api: user.api })); //토큰 전송
+            }
+        }
+    })
+})
+
 router.post('/check_is_our_g_user', (req, res) => {
-    // console.log("api : gChecker init.");
+    console.log(req);
     let userData = req.body;
     User.findOne({ email: userData.email }, (error, user) => {
         if (error) {
