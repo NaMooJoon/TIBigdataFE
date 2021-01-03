3afc3e7120334aab96992889c6c5d3527e795f0d
kksj216 <21900044@handong.edu>
2021-01-03 17:11:33 +0900

    [FEAT] Create service guide component

---
+++ src/app/modules/homes/body/intro/service-guide/service-guide.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-service-guide',
+  templateUrl: './service-guide.component.html',
+  styleUrls: ['./service-guide.component.less']
+})
+export class ServiceGuideComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
