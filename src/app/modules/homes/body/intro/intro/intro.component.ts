e695615f86c1ffc1c8cb529beb537d4b0245620f
kksj216 <21900044@handong.edu>
2021-01-03 17:11:08 +0900

    [FEAT] Create intro component

---
+++ src/app/modules/homes/body/intro/intro/intro.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-intro',
+  templateUrl: './intro.component.html',
+  styleUrls: ['./intro.component.less']
+})
+export class IntroComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+    this.test();
+
+  }
+
+  test() {
+    console.log("intro!");
+  }
+
+}
