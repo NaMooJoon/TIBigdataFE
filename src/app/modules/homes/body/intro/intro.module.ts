b1f446e3aaa84c1c4189bcfc742ae7f01653399d
kksj216 <21900044@handong.edu>
2021-01-03 17:13:16 +0900

    [FEAT] Set rounting for intro

--- src/app/modules/homes/body/intro/intro.module.ts
+++ src/app/modules/homes/body/intro/intro.module.ts
@@ -3,10 +3,14 @@ import { CommonModule } from '@angular/common';
 
 import { IntroRoutingModule } from './intro-routing.module';
 import { IntroRootComponent } from './intro-root/intro-root.component';
+import { IntroComponent } from './intro/intro.component';
+import { ServiceGuideComponent } from './service-guide/service-guide.component';
+import { CollectedInfoComponent } from './collected-info/collected-info.component';
+import { MemberPolicyComponent } from './member-policy/member-policy.component';
 
 
 @NgModule({
-  declarations: [IntroRootComponent],
+  declarations: [IntroRootComponent, IntroComponent, ServiceGuideComponent, CollectedInfoComponent, MemberPolicyComponent],
   imports: [
     CommonModule,
     IntroRoutingModule
