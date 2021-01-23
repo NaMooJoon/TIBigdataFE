c8484febd183084aa2306b7aaac165f5ec30981b
jeonyeohun <21500630@handong.edu>
2021-01-23 23:56:24 +0900

    [FIX] Delete unused declarations and set related keywords

--- src/app/modules/homes/body/search/search-result/search-result.component.ts
+++ src/app/modules/homes/body/search/search-result/search-result.component.ts
@@ -1,26 +1,7 @@
-import {
-  Component,
-  OnInit,
-  // ChangeDetectorRef,
-  // Input,
-  // Inject,
-  // Output
-} from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { Router } from "@angular/router";
 import { ElasticsearchService } from 'src/app/modules/communications/elasticsearch-service/elasticsearch.service';
-import { ArticleSource } from "../../shared-module/common-search-result-document-list/article/article.interface";
-import { Subscription } from "rxjs";
-// import { Observable, of } from "rxjs";
-import { IdControlService } from "../service/id-control-service/id-control.service";
-import { HttpClient, HttpHeaders } from "@angular/common/http";
-import { DocumentService } from "../service/document/document.service";
-
-import { IpService } from "src/app/ip.service";
-import { RecomandationService } from "../service/recommandation-service/recommandation.service";
 import { EPAuthService } from '../../../../communications/fe-backend-db/membership/auth.service';
-import { EventService } from "../../../../communications/fe-backend-db/membership/event.service";
-import { AnalysisDatabaseService } from "../../../../communications/fe-backend-db/analysis-db/analysisDatabase.service";
-
 
 @Component({
   selector: "app-search-result",
@@ -41,12 +22,11 @@ export class SearchResultComponent implements OnInit {
   ngOnInit() {
   }
 
-
   setRelatedKeywords(keys: string[]) {
     this.relatedKeywords = keys;
+
   }
 
-  //Get result from flask
   async freqAnalysis() {
     this._router.navigateByUrl("search/freqAnalysis");
   }
