403f47e37baa99d54f629e2667e290c4aac8ac02
jeonyeohun <21500630@handong.edu>
2021-01-30 23:08:25 +0900

    [FEAT] Add query model for elasticsearch

---
+++ src/app/modules/communications/elasticsearch-service/elasticsearch.service.query.model.ts
@@ -0,0 +1,72 @@
+export class ElasticSearchQueryModel {
+
+    private ids: string[] = [];
+    private searchKeyword: string = "";
+    private searchSource = [
+        "post_title",
+        "post_date",
+        "published_institution_url",
+        "published_institution",
+        "post_writer",
+        "post_body",
+        "file_download_url",
+    ];
+    private searchField: string[] = ["post_title", "file_extracted_content", "post_body"];
+    private filterPath: string[] = [
+        "hits.hits._source",
+        "hits.hits._id",
+        "hits.total",
+        "_scroll_id"
+    ]
+
+    private _allDocs = {
+        query: {
+            match_all: {}
+        }
+    };
+
+    public getAllDocs() {
+        return this._allDocs;
+    }
+
+    public getSearchDocs() {
+        return {
+            query: {
+                multi_match: {
+                    query: this.searchKeyword,
+                    fields: this.searchField,
+                }
+            }
+        };
+    }
+
+    public getSearchSource() {
+        return this.searchSource;
+    }
+
+    public getFilterPath() {
+        return this.filterPath;
+    }
+
+    public getSearchIds(ids: string | string[]) {
+        if (typeof (ids) === 'string') {
+            this.ids = [];
+            this.ids.push(ids);
+        }
+        else this.ids = ids;
+
+        return {
+            query: {
+                ids: {
+                    values: this.ids
+                }
+            }
+        };
+    }
+
+    public setSearchKeyword(keyword: string) {
+        console.log(keyword);
+        this.searchKeyword = keyword;
+        console.log(this.searchKeyword);
+    }
+}
