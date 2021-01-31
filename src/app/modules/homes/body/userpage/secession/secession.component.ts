71f70050028d4d8bcca04d94c2f8dfd93ff8e28e
kksj216 <21900044@handong.edu>
2021-01-31 16:09:45 +0900

    [FEAT] Add userpage secession component

---
+++ src/app/modules/homes/body/userpage/secession/secession.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-secession',
+  templateUrl: './secession.component.html',
+  styleUrls: ['./secession.component.less']
+})
+export class SecessionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
