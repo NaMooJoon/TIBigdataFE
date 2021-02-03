98a84273d2d82e5ad5a3ec8f6e86352f5db0e60a
jeonyeohun <21500630@handong.edu>
2021-02-03 23:05:31 +0900

    [FIX] fix component name into qna

---
+++ src/app/modules/homes/body/community/qna/qna.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from "@angular/router";
+import { CommunityService } from 'src/app/modules/communications/fe-backend-db/community/community.service';
+import { EPAuthService } from '../../../../communications/fe-backend-db/membership/auth.service';
+import { PaginationService } from '../../shared-services/pagination-service/pagination.service';
+
+@Component({
+  selector: 'app-qna',
+  templateUrl: './qna.component.html',
+  styleUrls: ['./qna.component.less']
+})
+export class QnaComponent implements OnInit {
+  constructor(
+    private router: Router,
+    private cmService: CommunityService,
+    private auth: EPAuthService,
+    private pgService: PaginationService,
+  ) { }
+
+  ngOnInit() {
+
+  }
+
+}
