fd05fa39f1409423a73a5e224cffebb66a901734
jeonyeohun <21500630@handong.edu>
2021-02-05 22:15:57 +0900

    [FIX] Add subscription of changing menu instead of update statically

--- src/app/modules/homes/body/community/community-root/community-root.component.ts
+++ src/app/modules/homes/body/community/community-root/community-root.component.ts
@@ -1,7 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { Router } from "@angular/router";
-
-export enum MENU { ANNCMNT, QNA, FAQ };
+import { boardMenu, CommunityService } from '../community-services/community.service';
 
 @Component({
   selector: 'app-community-root',
@@ -11,44 +10,39 @@ export enum MENU { ANNCMNT, QNA, FAQ };
 
 export class CommunityRootComponent implements OnInit {
 
-  constructor(private router: Router) { }
-  private selectedMenu: MENU;
+  constructor(
+    private router: Router,
+    private cmService: CommunityService,
+    private cdRef: ChangeDetectorRef) { }
+  private selectedMenu: boardMenu;
 
   ngOnInit() {
-    this.selectedMenu = MENU.ANNCMNT;
-  }
-  /**
-   * 
-   * 새 컴포넌트
-   * 글 제목
-   *  property title
-   * 글 내용
-   *  property content
-   * 저장
-   *  저장하는 함수
-   * 취소
-   *  취소하는 함수
-  */
 
+  }
+
+  ngAfterViewInit() {
+
+    this.cmService.getBoardMenuChange().subscribe(menu => {
+      this.selectedMenu = menu;
+      this.cdRef.detectChanges();
+    });
+
+  }
 
 
   navToQna() {
-    this.selectedMenu = MENU.QNA;
     this.router.navigateByUrl("community/qna");
   }
 
   navToAnnouncement() {
-    this.selectedMenu = MENU.ANNCMNT;
     this.router.navigateByUrl("community/announcement");
-
   }
 
   navToFaq() {
-    this.selectedMenu = MENU.FAQ;
     this.router.navigateByUrl("community/faq");
   }
 
-  public get menu(): typeof MENU {
-    return MENU
+  public get menu(): typeof boardMenu {
+    return boardMenu
   }
 }
