d8bf62a936fadf389d14e5556ab285edf5795cf5
jeonyeohun <21500630@handong.edu>
2021-02-05 09:44:19 +0900

    [FIX] Fix tsconfig to use import

--- src/app/modules/homes/body/community/qna/qna.component.ts
+++ src/app/modules/homes/body/community/qna/qna.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from "@angular/router";
-import { CommunityService } from 'src/app/modules/communications/fe-backend-db/community/community.service';
+import { CommunityService } from 'src/app/modules/homes/body/community/community-services/community.service';
 import { EPAuthService } from '../../../../communications/fe-backend-db/membership/auth.service';
 import { PaginationService } from '../../shared-services/pagination-service/pagination.service';
 
