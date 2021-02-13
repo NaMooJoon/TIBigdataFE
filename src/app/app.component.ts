a8c752556f4d494aa8fbae077f8f5537756afb61
jeonyeohun <21500630@handong.edu>
2021-02-14 02:24:40 +0900

    [FEAT] add loading flag for root component so that user and backend settings can be done before rendering other components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,28 @@
-import { Component } from '@angular/core';
-import { AuthService } from './modules/communications/fe-backend-db/membership/auth.service';
+import { Component } from "@angular/core";
+import { ElasticsearchService } from "./modules/communications/elasticsearch-service/elasticsearch.service";
+import { AuthService } from "./modules/communications/fe-backend-db/membership/auth.service";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.less']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.less"],
 })
 export class AppComponent {
-  title = 'KUBiC';
+  title = "KUBiC";
+  private isUserLoaded = false;
+  private isBackendAvailable = false;
 
-  constructor(private auth: AuthService) { }
+  constructor(
+    private authService: AuthService,
+    private elasticsearchSearvice: ElasticsearchService
+  ) {
+    this.isUserLoaded = false;
+    this.isBackendAvailable = false;
+  }
 
   async ngOnInit(): Promise<void> {
-    await this.auth.verifySignIn()
+    this.isBackendAvailable = await this.elasticsearchSearvice.isAvailable();
+    await this.authService.verifySignIn();
+    this.isUserLoaded = true;
   }
 }
