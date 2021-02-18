c224ad819e936bf44850647be9fbb7de6d97c60f
kksj216 <21900044@handong.edu>
2021-02-18 19:41:41 +0900

    [FEAT] Add load saved docs functions to my-docs

--- src/app/modules/homes/body/userpage/my-docs/my-docs.component.ts
+++ src/app/modules/homes/body/userpage/my-docs/my-docs.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../../../communications/fe-backend-db/membership/auth.service';
+import { PaginationService } from "src/app/modules/homes/body/shared-services/pagination-service/pagination.service"
+import { DocumentService } from 'src/app/modules/homes/body/shared-services/document-service/document.service';
+import { UserDocumentService } from '../../../../communications/fe-backend-db/userDocument/userDocument.service';
+import { PaginationModel } from "../../shared-services/pagination-service/pagination.model";
 
 @Component({
   selector: 'app-my-docs',
@@ -8,18 +12,61 @@ import { AuthService } from '../../../../communications/fe-backend-db/membership
 })
 export class MyDocsComponent implements OnInit {
 
-  private myDocs: string[] = [];
-  private isDocEmpty: boolean = false;
-
   constructor(
     private _auth: AuthService,
-
+    private paginationService: PaginationService,
+    private userDocumentService: UserDocumentService,
   ) { }
+
+  private isSavedDocsLoaded = false;
+  private savedDocs: Array<{ title: string; id: string }>;
+  private pageInfo: PaginationModel;
+  private totalSavedDocsNum: number;
 
   ngOnInit(): void {
 
+    this.loadSavedDocs(1);
+
+  }
+
+  async loadSavedDocs(pageNum: number): Promise<void> {
+    this.isSavedDocsLoaded = false;
+    this.totalSavedDocsNum = await this.userDocumentService.getTotalDocNum();
+    pageNum = this.handlePageOverflow(pageNum);
+    this.savedDocs = await this.userDocumentService.getMyDocs(pageNum);
+    this.pageInfo = await this.paginationService.paginate(
+      pageNum,
+      this.totalSavedDocsNum,
+      10,
+      3
+    );
+
+    console.log(this.pageInfo);
+    this.isSavedDocsLoaded = true;
+  }
+
+  handlePageOverflow(pageNum: number): number {
+    if (pageNum < 0) pageNum = 1;
+    else if (pageNum > this.totalSavedDocsNum) pageNum = this.totalSavedDocsNum;
+
+    return pageNum;
+  }
 
 
+  selectedStyleObject(flag: boolean): Object {
+    if (flag) {
+      return {
+        color: "white",
+        "background-color": "#0FBAFF",
+        border: "none",
+        "font-weight": "700",
+      };
+    } else {
+      return {
+        color: "black",
+        "background-color": "white",
+      };
+    }
   }
 
 }
