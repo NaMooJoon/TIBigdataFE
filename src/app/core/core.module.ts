8351aa34283b8a6c4ee8437384a10d15cd0615b6
Jeon, Yeo Hun <21500630@handong.edu>
2021-02-27 20:23:02 +0900

    [STYLE] Reorganized imports

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,27 +1,21 @@
-import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
-
-//Services
+import { NgModule } from "@angular/core";
+import { GoogleLoginProvider, SocialAuthServiceConfig, SocialLoginModule } from "angularx-social-login";
+import { FooterComponent } from "./components/footer/footer.component";
+import { NavbarComponent } from "./components/navbar/navbar.component";
+import { AuthGuard } from "./guards/auth-guard/auth.guard";
+import { TokenInterceptorService } from "./interceptor/token-interceptor.service";
+import { ElasticSearchQueryModel } from "./models/elasticsearch.service.query.model";
 import { AnalysisDatabaseService } from "./services/analysis-database-service/analysis.database.service";
+import { ArticleService } from "./services/article-service/article.service";
+import { AuthenticationService } from "./services/authentication-service/authentication.service";
 import { ElasticsearchService } from "./services/elasticsearch-service/elasticsearch.service";
 import { IpService } from "./services/ip-service/ip.service";
 import { PaginationService } from "./services/pagination-service/pagination.service";
 import { UserSavedDocumentService } from "./services/user-saved-document-service/user-saved-document.service";
 import { WordcloudService } from "./services/wordcloud-service/wordcloud.service";
-import { AuthenticationService } from "./services/authentication-service/authentication.service";
-import { ArticleService } from "./services/article-service/article.service";
 
-//components
-import { NavbarComponent } from "./components/navbar/navbar.component";
-import { FooterComponent } from "./components/footer/footer.component";
-import { AuthGuard } from "./guards/auth-guard/auth.guard";
-import { TokenInterceptorService } from "./interceptor/token-interceptor.service";
-import {
-  GoogleLoginProvider,
-  SocialAuthServiceConfig,
-  SocialLoginModule,
-} from "angularx-social-login";
-import { ElasticSearchQueryModel } from "./models/elasticsearch.service.query.model";
+
 
 // Google API ID
 export const PROVIDER_ID: string =
@@ -57,4 +51,4 @@ export const PROVIDER_ID: string =
     },
   ],
 })
-export class CoreModule {}
+export class CoreModule { }
