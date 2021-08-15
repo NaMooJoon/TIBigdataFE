7c213c08c5b8befc27f404d568dc5a08dac79fab
cinxdy <cinxdys2@gmail.com>
2021-08-15 14:43:48 +0900

    [STYLE] Update question mark icon and update the button style

--- src/app/features/article-analysis/components/analysis/analysis.component.ts
+++ src/app/features/article-analysis/components/analysis/analysis.component.ts
@@ -28,19 +28,22 @@ export class AnalysisComponent implements OnInit  {
   }
 
   showPop(analName:string){
-    document.getElementById(analName).style.display='inline';
+    if(document.getElementById(analName).style.display=='inline')
+      document.getElementById(analName).style.display='none'
+    else
+      document.getElementById(analName).style.display='inline';
   }
 
-  async runAnalysisCount(): Promise<void> {
+  async runAnylysis(activity:string): Promise<void>{
 
-    let optionValue = (<HTMLInputElement> document.getElementById('count_option1')).value ;
+    let optionValue =  (<HTMLInputElement> document.getElementById(activity+'_option1')).value ;
 
     let data = JSON.stringify({
       'userEmail': this.email, 
       'keyword': this.selectedKeyword, 
       'savedDate': this.selectedSavedDate,
       'optionList': optionValue,
-      'analysisName': 'count',
+      'analysisName': activity,
     });
     
     console.log(data);
@@ -48,49 +51,68 @@ export class AnalysisComponent implements OnInit  {
     
     this.AnalysisResult = res.result;
     this.isDataAnalysised = true;
-    
   }
 
-  async runAnalysisTfidf(): Promise<void> {
+  // async runAnalysisCount(): Promise<void> {
 
-    let optionValue = (<HTMLInputElement> document.getElementById('tfidf_option1')).value ;
+  //   let optionValue = (<HTMLInputElement> document.getElementById('count_option1')).value ;
 
-    let data = JSON.stringify({
-      'userEmail': this.email, 
-      'keyword': this.selectedKeyword, 
-      'savedDate': this.selectedSavedDate,
-      'optionList': optionValue,
-      'analysisName': 'tfidf',
-    });
+  //   let data = JSON.stringify({
+  //     'userEmail': this.email, 
+  //     'keyword': this.selectedKeyword, 
+  //     'savedDate': this.selectedSavedDate,
+  //     'optionList': optionValue,
+  //     'analysisName': 'count',
+  //   });
     
-    console.log(data);
-    let res = await this.middlewareService.postDataToMiddleware('/textmining',data);
+  //   console.log(data);
+  //   let res = await this.middlewareService.postDataToMiddleware('/textmining',data);
     
-    this.AnalysisResult = res.result;
-    this.isDataAnalysised = true;
+  //   this.AnalysisResult = res.result;
+  //   this.isDataAnalysised = true;
     
-  }
+  // }
+
+  // async runAnalysisTfidf(): Promise<void> {
+
+  //   let optionValue = (<HTMLInputElement> document.getElementById('tfidf_option1')).value ;
+
+  //   let data = JSON.stringify({
+  //     'userEmail': this.email, 
+  //     'keyword': this.selectedKeyword, 
+  //     'savedDate': this.selectedSavedDate,
+  //     'optionList': optionValue,
+  //     'analysisName': 'tfidf',
+  //   });
+    
+  //   console.log(data);
+  //   let res = await this.middlewareService.postDataToMiddleware('/textmining',data);
+    
+  //   this.AnalysisResult = res.result;
+  //   this.isDataAnalysised = true;
+    
+  // }
 
 
-  async runAnalysisNetwork(): Promise<void> {
+  // async runAnalysisNetwork(): Promise<void> {
 
-    let optionValue = (<HTMLInputElement> document.getElementById('network_option1')).value ;
+  //   let optionValue = (<HTMLInputElement> document.getElementById('network_option1')).value ;
 
-    let data = JSON.stringify({
-      'userEmail': this.email, 
-      'keyword': this.selectedKeyword, 
-      'savedDate': this.selectedSavedDate,
-      'optionList': optionValue,
-      'analysisName': 'network',
-    });
+  //   let data = JSON.stringify({
+  //     'userEmail': this.email, 
+  //     'keyword': this.selectedKeyword, 
+  //     'savedDate': this.selectedSavedDate,
+  //     'optionList': optionValue,
+  //     'analysisName': 'network',
+  //   });
     
-    console.log(data);
-    let res = await this.middlewareService.postDataToMiddleware('/textmining',data);
+  //   console.log(data);
+  //   let res = await this.middlewareService.postDataToMiddleware('/textmining',data);
     
-    this.AnalysisResult = res.result;
-    this.isDataAnalysised = true;
+  //   this.AnalysisResult = res.result;
+  //   this.isDataAnalysised = true;
     
-  }
+  // }
 
 
 }
