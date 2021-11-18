2e4ebd391ba2aaac4d0dedda600c1691ced0611b
cinxdy <cinxdys2@gmail.com>
2021-11-18 21:39:54 +0900

    [REFACTORING] Update the descriptions for several functions

--- src/app/features/open-api/components/management/management.component.ts
+++ src/app/features/open-api/components/management/management.component.ts
@@ -11,9 +11,10 @@ import { AuthenticationService } from "src/app/core/services/authentication-serv
   styleUrls: ["./management.component.less"],
 })
 export class ManagementComponent implements OnInit {
-  userProfile: UserProfile;
-  name:string;
-  count: number;
+  private _userProfile: UserProfile;
+  private _name: string;
+  private _count: number;
+  
   public infos: { app_name: string; app_purpose: string; user_email: string; reporting_date: string; expiration_date: string; traffic: number; }[];
 
   constructor(
@@ -30,14 +31,21 @@ export class ManagementComponent implements OnInit {
     this.loadInfo();
   }
 
+  /**
+   * @description load API info from the middleware by user email
+   */
   async loadInfo(){
     let info=await this.apiService.getApiInfos(this.userProfile.email);
     this.count = info.count;
     this.infos= info.info;
-    console.log(this.infos);
+    // console.log(this.infos);
     this.name=this.userProfile.name;
   }
 
+  /**
+   * @description reissue the authentication key of the selected application
+   * @param _id
+   */
   async reissue(_id:string): Promise<void>{
     let authKey = (await this.apiService.reissueKey(_id)).authKey;
     prompt('성공적으로 인증키가 변경되었습니다.\n복사하세요.',authKey);
@@ -45,6 +53,10 @@ export class ManagementComponent implements OnInit {
     return ;
   }
 
+  /**
+   * @description delete the selected application
+   * @param _id
+   */
   async delete(_id:string): Promise<void>{
     if ((await this.apiService.delete(_id)).succeed)
       alert('성공적으로 삭제되었습니다');
@@ -53,9 +65,32 @@ export class ManagementComponent implements OnInit {
     return ;
   }
 
+  /**
+   * @description move to the register page to register a user's API application
+   */
   toRegister(){
     this.router.navigateByUrl("/openapi/register");
     this.ngOnInit();
   }
 
+  public get userProfile(): UserProfile {
+    return this._userProfile;
+  }
+  public set userProfile(value: UserProfile) {
+    this._userProfile = value;
+  }
+
+  public get name(): string {
+    return this._name;
+  }
+  public set name(value: string) {
+    this._name = value;
+  }
+
+  public get count(): number {
+    return this._count;
+  }
+  public set count(value: number) {
+    this._count = value;
+  }
 }
