49e41354a19fa8a3db2ae994d002fb34d9acf5df
SUJIN YANG <yangmit0524@gmail.com>
2022-01-05 16:07:04 +0900

    [CHORE] update npm dependencies and remove backup files of fe-backend

--- fe-backend/back/models/Res.js
+++
@@ -1,9 +0,0 @@
-class QueryResponse {
-  constructor(isSuccess, responseMessage, payload) {
-    this.isSuccess = isSuccess;
-    this.responseMessage = responseMessage;
-    this.payload = payload;
-  }
-}
-
-module.exports = QueryResponse;
